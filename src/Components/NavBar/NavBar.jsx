21427b2700b6f2d6f4cf7165097ea99a77bfe751
Sebastian Serruto <sebastianserruto@gmail.com>
2024-02-29 17:42:54 -0300

    Creacion del menu hamburgesa y medias querys

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,19 +2,36 @@ import {Link, NavLink} from 'react-router-dom';
 import CardWidget from '../CarWidget/CardWidget';
 import './NavBar.css';
 import Logo from '../Logo/Logo';
+import { useState } from 'react';
 
 function NavBar() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const closeMenu = () => {
+        setIsActive(false);
+    };
+
     return (
         <header>
             <Link to="/">
                 <Logo />
             </Link>
-            <nav>
-                <NavLink to="/categoria/consoles">Consolas</NavLink>
-                <NavLink to="/categoria/games">Juegos</NavLink>
-                <NavLink to="/categoria/accesories">Accesorios</NavLink>
+            <div className='navbarD'>
+                <nav>
+                    <div className={`navLinks ${isActive && "active"}`}>
+                    <NavLink to="/categoria/consoles" onClick={closeMenu}>Consolas</NavLink>
+                    <NavLink to="/categoria/games" onClick={closeMenu}>Juegos</NavLink>
+                    <NavLink to="/categoria/accesories" onClick={closeMenu}>Accesorios</NavLink>
+                    </div>
+                    <div className={`navToggle ${isActive && "active"}`} onClick={ () => setIsActive (!isActive)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </nav>
                 <CardWidget />
-            </nav>
+            </div>
         </header>
 
 );
